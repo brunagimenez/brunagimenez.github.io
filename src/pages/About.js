619bb17939a61a2ec6a70e50e50a0f4f011b9ed5
Bruna <brunagimenez97@gmail.com>
2023-04-17 13:26:53 -0300

    habilidades

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,55 +13,22 @@ export default function About() {
         {translation
           ? <h1>About me</h1>
           : <h1>Sobre Mim</h1>}
-        {translation
-          ? <>
-            <p>
-              I am transitioning my career from Designer to Full Stack Developer.
-              In the Design field, I gained valuable knowledge and wonderful
-              experiences that are helping me in programming. My soft skills
-              developed throughout my career, such as communication with clients,
-              have also proved to be valuable in programming.
-            </p>
-            <p>
-              Some of the tools I have been working with are:
-            </p>
-          </>
-          : <>
-            <p>
-              Estou em transição de carreira de Designer para Desenvolvedor Full Stack.
-              Na área de Design, adquiri conhecimentos valiosos e experiências
-              maravilhosas que estão me ajudando na programação. Minhas
-              habilidades interpessoais desenvolvidas ao longo da carreira,
-              como comunicação com clientes, também se mostraram valiosas na programação.
-            </p>
-            <p>
-              Algumas das ferramentas com as quais tenho trabalhado são:
-            </p>
-          </>}
-        <ul>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>Node.js</li>
-          <li>Jest</li>
-          <li>Framework (Bootstrap)</li>
-          <li>SASS</li>
-          <li>CSS</li>
-        </ul>
-        {(translation)
-          ? (<p>
-            I also use Agile methodologies and some tools that assist me such as:
-          </p>)
-          : <p>
-            E também uso as Metodologias Ágeis, e algumas
-            ferramentas que me auxiliam como:
-          </p>}
-        <ul>
-          <li>Scrum</li>
-          <li>Kanban</li>
-          <li>Trello</li>
-          <li>Notion</li>
-          <li>Asana</li>
-        </ul>
+        {/* {translation
+          ? <p>
+            I am transitioning my career from Designer to Full Stack Developer.
+            In the Design field, I gained valuable knowledge and wonderful
+            experiences that are helping me in programming. My soft skills
+            developed throughout my career, such as communication with clients,
+            have also proved to be valuable in programming.
+          </p>
+          :  */}
+        <p>
+          Estou em transição de carreira de Designer para Desenvolvedor Full Stack.
+          Na área de Design, adquiri conhecimentos valiosos e experiências
+          maravilhosas que estão me ajudando na programação. Minhas
+          habilidades interpessoais desenvolvidas ao longo da carreira,
+          como comunicação com clientes, também se mostraram valiosas na programação.
+        </p>
       </main>
     </>
   );
--- src/pages/Soft.js
+++ src/pages/Soft.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import MenuAside from '../components/MenuAside';
 import Header from '../components/Header';
+import imgJS from '../image/javascript.svg';
+import imgHTML from '../image/html.svg';
+import imgReact from '../image/react.svg';
+import imgRedux from '../image/redux.svg';
+import imgTesting from '../image/testing.svg';
+import imgSASS from '../image/sass.svg';
+import imgCSS from '../image/css.svg';
 
 export default function Soft() {
   return (
@@ -9,6 +16,36 @@ export default function Soft() {
       <Header />
       <main>
         <h1>Habilidades</h1>
+        <section className="softIMG">
+          <div>
+            <h2>JavaScript</h2>
+            <img src={ imgJS } alt="JavaScript" />
+          </div>
+          <div>
+            <h2>HTML5</h2>
+            <img src={ imgHTML } alt="HTML5" />
+          </div>
+          <div>
+            <h2>React</h2>
+            <img src={ imgReact } alt="React" />
+          </div>
+          <div>
+            <h2>Redux</h2>
+            <img src={ imgRedux } alt="Redux" />
+          </div>
+          <div>
+            <h2>React Testing</h2>
+            <img src={ imgTesting } alt="React Testing" />
+          </div>
+          <div>
+            <h2>SASS</h2>
+            <img src={ imgSASS } alt="SASS" />
+          </div>
+          <div>
+            <h2>CSS3</h2>
+            <img src={ imgCSS } alt="CSS3" />
+          </div>
+        </section>
       </main>
     </>
   );
