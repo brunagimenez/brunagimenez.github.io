44737e0d5f78786a597aa5e19ff5698f1ad1c774
Bruna <brunagimenez97@gmail.com>
2023-04-16 23:22:43 -0300

    iniciado

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import MenuAside from '../components/MenuAside';
 import Header from '../components/Header';
+import { TranslationContext } from '../context/TranslationContext';
 
 export default function Home() {
+  const { translation } = useContext(TranslationContext);
   return (
     <>
       <Header />
       <MenuAside />
       <main>
-        <div>
-          <h1>Bruna</h1>
+        <div className="home-name">
+          <h1 className="home-firstname">Bruna</h1>
           <h1>Gimenez</h1>
         </div>
-        <div>
-          <h2>Desenvolvedora</h2>
-          <h2>Front-End</h2>
+        <div className="home-dev">
+          {translation
+            ? <h2>Developer</h2>
+            : <h2>Desenvolvedora</h2>}
+
+          <h2 className="home-stack">Front-End.</h2>
         </div>
         <div>
-          <button>Dowloand CV</button>
-          <button>Contato</button>
+          <button>Download CV</button>
+          {translation
+            ? <button className="home-button"> Contact </button>
+            : <button className="home-button"> Contato </button>}
         </div>
       </main>
     </>
