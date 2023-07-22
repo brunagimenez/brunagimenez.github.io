4c19281afa373db72f215cbe72e3bbec771b3d1a
Bruna <brunagimenez97@gmail.com>
2023-07-21 22:06:02 -0300

    animation

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,22 +13,23 @@ export default function About() {
         {translation
           ? <h1>About me</h1>
           : <h1>Sobre Mim</h1>}
-        {/* {translation
-          ? <p>
+        {translation ? (
+          <p className="about">
             I am transitioning my career from Designer to Full Stack Developer.
             In the Design field, I gained valuable knowledge and wonderful
             experiences that are helping me in programming. My soft skills
             developed throughout my career, such as communication with clients,
             have also proved to be valuable in programming.
           </p>
-          :  */}
-        <p>
-          Estou em transição de carreira de Designer para Desenvolvedor Full Stack.
-          Na área de Design, adquiri conhecimentos valiosos e experiências
-          maravilhosas que estão me ajudando na programação. Minhas
-          habilidades interpessoais desenvolvidas ao longo da carreira,
-          como comunicação com clientes, também se mostraram valiosas na programação.
-        </p>
+        ) : (
+          <p className="about">
+            Estou em transição de carreira de Designer para Desenvolvedor Full Stack.
+            Na área de Design, adquiri conhecimentos valiosos e experiências
+            maravilhosas que estão me ajudando na programação. Minhas
+            habilidades interpessoais desenvolvidas ao longo da carreira,
+            como comunicação com clientes, também se mostraram valiosas na programação.
+          </p>
+        )}
       </main>
     </>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 import MenuAside from '../components/MenuAside';
 import Header from '../components/Header';
 import { TranslationContext } from '../context/TranslationContext';
+import imgBruna from '../image/brunaFoto.jpeg';
 
 const link = 'https://drive.google.com/file/d/1amQ2ouDutDdE0J6qeBFKk9v1wBe1Aoha/view?usp=drive_link';
 
@@ -21,24 +22,35 @@ export default function Home() {
     <>
       <Header />
       <MenuAside />
-      <main>
-        <div className="home-name">
-          <h1 className="home-firstname">Bruna</h1>
-          <h1>Gimenez</h1>
-        </div>
-        <div className="home-dev">
-          {translation
-            ? <h2>Developer</h2>
-            : <h2>Desenvolvedora</h2>}
+      <main className="main-home">
+        <section>
+          <h1 className="to-appear-animation">Bruna Gimenez</h1>
+          <div className="home-name animation-type typing-animation">
+            {translation
+              ? <h1>Hi! I&apos;m</h1>
+              : <h1>Olá! Eu sou a </h1>}
 
-          <h2 className="home-stack">Front-End.</h2>
-        </div>
-        <div>
-          <button onClick={ handleButtonClick }>Download CV</button>
-          {translation
-            ? <button className="home-button" onClick={ navToContact }> Contact </button>
-            : <button className="home-button" onClick={ navToContact }> Contato </button>}
-        </div>
+            <h1 className="home-firstname">Bruna</h1>
+            <h1>Gimenez</h1>
+          </div>
+          <div className="home-dev">
+            {translation
+              ? <h2>Developer</h2>
+              : <h2>Desenvolvedora</h2>}
+
+            <h2 className="home-stack">Full Stack</h2>
+          </div>
+          <div>
+            <button onClick={ handleButtonClick }>Download CV</button>
+            <button className="home-button" onClick={ navToContact }>
+              {translation ? 'Contact' : 'Contato' }
+            </button>
+          </div>
+        </section>
+        <section>
+          <div className="bd" />
+          <img src={ imgBruna } className="shape" alt="Foto Bruna" width="550px" />
+        </section>
       </main>
     </>
   );
